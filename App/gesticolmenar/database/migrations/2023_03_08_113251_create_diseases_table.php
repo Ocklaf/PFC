a596ea5ea83e13c2b88994e85770d8881b25deed
Ocklaf <ocklaf@gmail.com>
2023-04-23 13:50:22 +0200

    Terminados los mockups de PC, empezando Mobil

--- App/gesticolmenar/database/migrations/2023_03_08_113251_create_diseases_table.php
+++ App/gesticolmenar/database/migrations/2023_03_08_113251_create_diseases_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->date('treatment_start_date');
             $table->date('treatment_repeat_date');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('beehive_id')->references('id')->on('beehives');
+            $table->foreign('beehive_id')->references('id')->on('beehives')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
